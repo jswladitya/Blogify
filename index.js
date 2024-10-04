ef83677f0c0e15dd8c47d10fd4391e17182d0320
jswladitya <jswladitya0403@gmail.com>
2024-10-05 00:10:01 +0530

    Project files added

---
+++ index.js
@@ -0,0 +1,43 @@
+
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookiePaser = require("cookie-parser");
+
+mongoose.set('strictQuery', true);
+const Blog = require("./models/blog.models");
+
+const userRoute = require("./routes/user.routes");
+const blogRoute = require("./routes/blog.routes");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const app = express();
+const PORT = 8000;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/kisan")
+  .then((e) => console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
